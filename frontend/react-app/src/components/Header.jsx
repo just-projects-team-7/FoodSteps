d29e1309603130e35999219bdb9e1846096d1632
Adeola Fadare <adeolafadare8@gmail.com>
2022-10-17 22:59:21 +0100

    write for me initialised

--- frontend/react-app/src/components/Header.jsx
+++ frontend/react-app/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Navigate } from "react-router-dom";
 
 import Overlay from "./Overlay";
 
@@ -191,40 +191,102 @@ class Header extends Component {
   }
 }
 class Search extends Component {
-  state = {};
+  state = {
+    input: "",
+    redirect: null 
+  };
+  displaySearchResults(obj) {
+    if (!obj.error) {
+	    let { category, title } = obj
+	    this.setState({
+	      redirect: `/category/${category}/${title}`
+	    })
+    }
+    
+  }
+  async getPosts(param) {
+    let options = {
+      // Adding method type
+      method: "GET",
+      // Adding body or contents to send
+      /*body: JSON.stringify({
+          title: "foo",
+          body: "bar",
+          userId: 1
+      }),*/
+      // Adding headers to the request
+      headers: {
+          "Content-type": "application/json; charset=UTF-8"
+      }
+    }
+    var response =  await fetch(`https://davinci.pythonanywhere.com/posts/${param}`, options)
+    if (!response.error) {
+      var data = await response.json()
+      console.log(data);
+      if (data.title === "") {
+        console.log('Not Found');
+        this.displaySearchResults({error: 'No results'})
+        return
+      }
+      this.displaySearchResults(data)
+      
+    }
+    else {
+      console.log(response.error)
+      return (response.error)
+    }
+  }
   render() {
     return (
-      <div className="search">
-        <div className="search-box">
-          <span>
-            <input
-              type="text"
-              name="blog-search"
-              className="blog-search"
-              placeholder="search"
-            />
-          </span>
-          <span className="svg-wrapper">
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11 20.75C5.62 20.75 1.25 16.38 1.25 11C1.25 5.62 5.62 1.25 11 1.25C16.38 1.25 20.75 5.62 20.75 11C20.75 16.38 16.38 20.75 11 20.75ZM11 2.75C6.45 2.75 2.75 6.45 2.75 11C2.75 15.55 6.45 19.25 11 19.25C15.55 19.25 19.25 15.55 19.25 11C19.25 6.45 15.55 2.75 11 2.75Z"
-                fill="#858688"
+      <React.Fragment>
+        <div className="search">
+          <div className="search-box">
+            <span>
+              <input
+                type="text"
+                name="blog-search"
+                className="blog-search"
+                placeholder="search"
+                onInput={
+                  (e) => {
+                    console.log(e.target.value);
+                    this.setState({
+                      input: e.target.value,
+                    })
+                  }
+                }
               />
-              <path
-                d="M20.1601 22.79C20.0801 22.79 20.0001 22.78 19.9301 22.77C19.4601 22.71 18.6101 22.39 18.1301 20.96C17.8801 20.21 17.9701 19.46 18.3801 18.89C18.7901 18.32 19.4801 18 20.2701 18C21.2901 18 22.0901 18.39 22.4501 19.08C22.8101 19.77 22.7101 20.65 22.1401 21.5C21.4301 22.57 20.6601 22.79 20.1601 22.79ZM19.5601 20.49C19.7301 21.01 19.9701 21.27 20.1301 21.29C20.2901 21.31 20.5901 21.12 20.9001 20.67C21.1901 20.24 21.2101 19.93 21.1401 19.79C21.0701 19.65 20.7901 19.5 20.2701 19.5C19.9601 19.5 19.7301 19.6 19.6001 19.77C19.4801 19.94 19.4601 20.2 19.5601 20.49Z"
-                fill="#858688"
-              />
-            </svg>
-          </span>
+            </span>
+            <span
+             className="svg-wrapper"
+             onClick={
+              () => {
+                this.getPosts(this.state.input)
+              }
+             }
+             >
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11 20.75C5.62 20.75 1.25 16.38 1.25 11C1.25 5.62 5.62 1.25 11 1.25C16.38 1.25 20.75 5.62 20.75 11C20.75 16.38 16.38 20.75 11 20.75ZM11 2.75C6.45 2.75 2.75 6.45 2.75 11C2.75 15.55 6.45 19.25 11 19.25C15.55 19.25 19.25 15.55 19.25 11C19.25 6.45 15.55 2.75 11 2.75Z"
+                  fill="#858688"
+                />
+                <path
+                  d="M20.1601 22.79C20.0801 22.79 20.0001 22.78 19.9301 22.77C19.4601 22.71 18.6101 22.39 18.1301 20.96C17.8801 20.21 17.9701 19.46 18.3801 18.89C18.7901 18.32 19.4801 18 20.2701 18C21.2901 18 22.0901 18.39 22.4501 19.08C22.8101 19.77 22.7101 20.65 22.1401 21.5C21.4301 22.57 20.6601 22.79 20.1601 22.79ZM19.5601 20.49C19.7301 21.01 19.9701 21.27 20.1301 21.29C20.2901 21.31 20.5901 21.12 20.9001 20.67C21.1901 20.24 21.2101 19.93 21.1401 19.79C21.0701 19.65 20.7901 19.5 20.2701 19.5C19.9601 19.5 19.7301 19.6 19.6001 19.77C19.4801 19.94 19.4601 20.2 19.5601 20.49Z"
+                  fill="#858688"
+                />
+              </svg>
+            </span>
+          </div>
+          <div className="search-results"></div>
         </div>
-        <div className="search-results"></div>
-      </div>
+        {this.state.redirect ? <Navigate to={this.state.redirect}/> : null}
+      </React.Fragment>
     );
   }
 }
